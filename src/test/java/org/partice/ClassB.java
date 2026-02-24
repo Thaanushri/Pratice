ab5d460274e4dd6c4e1cc6baf174edc36b6f1f27
Thaanushri <thaanushrishri@gmail.com>
2026-02-24 10:13:15 +0530

    Pattu

---
+++ src/test/java/org/partice/ClassB.java
@@ -0,0 +1,14 @@
+package org.partice;
+
+public class ClassB {
+
+	public void m4() {
+		System.out.println("Hello 1");
+	}
+	public void m5() {
+		System.out.println("Hi 1");
+	}
+	public void m6() {
+		System.out.println("Hey 1");
+	}
+}
